d5d4192a30ed3b2113f1d4af5630930330accb91
Ezihale <ezihale@gmail.com>
2022-12-10 05:08:49 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - prints a given phrase
- * Return: o if existed properly, non-zero
+ * main - Entry
+ * Return: Always 0
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
+}
