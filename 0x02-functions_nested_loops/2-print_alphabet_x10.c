22d868c15178ecab9e48c6863cbdcce2e16cf6a9
Ezihale <ezihale@gmail.com>
2022-12-14 04:58:32 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print= prints 10 times the alphabet-Entry point
+ *
+ * Return: 0
+ */
+void prints 10 times the alphabet(void)
+
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		char c = '1'
+
+		while (c <= '10')
+		{
+			c++;
+		}
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n')
+}
